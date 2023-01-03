668df6a8fd58540314c5528bba2d5e0266276825
jaygala <jaygala260@gmail.com>
2023-01-03 09:44:46 +0530

    added mpa starter code

---
+++ pages/mpa.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Checking for possible MPA intrusion")
+
+st.write("Enter the coordinates, time and date of the image uploaded")
+
+time = st.time_input("Time of clicking the image")
+
+st.date_input("Enter the date of clicking the image")
+
+st.number_input("Enter coordinates: (in decimal degree format)")
+st.number_input("Enter coordinates: S")
+
+# point_conception = {"LAT":[
+# 34° 27.00' N. lat,
+# "34° 27.00' N. lat",
+# "34° 23.96' N. lat",
+# "34° 27.19' N. lat"], 
+
+# "LON":["120° 28.28' W. long", "120° 32.15' W. long", "120° 25.00' W. long", "120° 25.00' W. long"],
+# }
+
+# mpa_df = pd.DataFrame(point_conception)
+
+# st.map(mpa_df)
+st.map()
